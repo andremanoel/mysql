34d7a21a374cb1767015dc29ca4b7f00e7d68005
andresouza <andrezaum20@gmail.com>
2017-12-19 17:18:40 -0200

    Atualizando sql

--- criar-banco.sql
+++ criar-banco.sql
@@ -15,5 +15,16 @@ CREATE TABLE `treinamento`.`tb_aluno` (
 SELECT * FROM treinamento.tb_aluno;
 
 -- INSERINDO UM ALUNO
-
 INSERT INTO `treinamento`.`tb_aluno` (`nome`, `data_nascimento`) VALUES ('André Manoel', '1985-01-01');
+INSERT INTO `treinamento`.`tb_aluno` (`nome`, `data_nascimento`) VALUES ('Marcondes', '1970-02-24');
+
+-- Criando Tabela Matéria
+CREATE TABLE `treinamento`.`tb_materia` (
+  `co_materia` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NULL,
+  PRIMARY KEY (`co_materia`));
+
+  
+
+
+
